828f289a4e66f7ff2b5c7bb6cacb5cf2c380414d
Honza S. Copak <bonum.comunae@volny.cz>
2016-10-28 09:44:41 +0200

    Update dallas1.ino

--- dallas1.ino
+++ dallas1.ino
@@ -1,25 +1,15 @@
-//
+//octopusengine - basic test: simple dallas temperature sensor
 //-----------------------------------------------------------
-#include <OneWire.h> //2016/10 - bylo nutno stáhnout novou verzi:
-//https://github.com/PaulStoffregen/OneWire
-#include <DallasTemperature.h> // knihovna pro sensor DS18B20
-#include <Wire.h> //pro i2c a oled
+#include <OneWire.h> //2016/10 - new edition: https://github.com/PaulStoffregen/OneWire
+#include <DallasTemperature.h> // lib. for DS18B20
+#include <Wire.h> //for i2c / oled
 #include "oeESP.h"
-
 // ------ declaration DS18B20
 #define ONE_WIRE_BUS 0
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 //------ /declar
-
-int interval=2;
-//------------------------------------------------------------
-float minC = 199;
-float maxC = -99;
-float minC1 = 199;
-float maxC1 = -99;
-float minC2 = 199;
-float maxC2 = -99;
+int interval=2; // every 2 sec.
 float celsius;
 
 unsigned long previousMillis = 0;
@@ -34,21 +24,17 @@ void setup() {
 
 void loop() {
   blinkBlu();
-  
   unsigned long currentMillis = millis();
   
   if (currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
+     previousMillis = currentMillis;
+     sensors.requestTemperatures(); // Send the command to get temperatures
+     celsius = sensors.getTempCByIndex(0);
 
-    // ------ DS18B20
-    sensors.requestTemperatures(); // Send the command to get temperatures
-    celsius = sensors.getTempCByIndex(0);
-
-    Serial.println(celsius);
-    if (int(celsius)>25) 
-    {
-    ledRed(); 
-     delay(500);  
+     Serial.println(celsius);
+     if (int(celsius)>25){
+       ledRed(); 
+       delay(500);  
      } 
    }
 }
